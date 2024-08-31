043ff01ff316bd190631fe7c06b2c26106d2de98
wy2898 <weiyi0124@gmail.com>
2024-08-31 15:01:33 -0400

    implement home page and sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = {
+    firstName: "Yi",
+    lastName: "Wei",
+    email: "weiyi0124@gmail.com",
+  };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1234.56}
+          />
+        </header>
+        RESENT TRANSACTIONS
+      </div>
+
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 198.5 }, { currentBalance: 890.5 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
